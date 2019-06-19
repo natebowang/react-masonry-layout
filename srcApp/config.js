6d8cd514b0436e1c93a3caf0bbf06849992bde47
natebowang <120029790@qq.com>
2019-06-19 09:32:53 +0800

    Refactor wiw into config.js

--- srcApp/config.js
+++ srcApp/config.js
@@ -1,3 +1,11 @@
-export const HALF_GAP = 1; // rem, half half between cells in masonry layout
-export const MIN_COLUMN_NO = 1; // Column number can not less than this value
-export const DEFAULT_COLUMN_WIDTH= 20; // rem
+// half half between cells in masonry layout
+export const HALF_GAP = 1; // rem
+
+// Column number can not less than this value
+export const MIN_COLUMN_NO = 1;
+
+// If window inner width less than this, column width will
+// larger than DEFAULT_COLUMN_WIDTH to extend to the maximum use of the window.
+export const WIW_THRESHOLD = 900; // px
+
+export const DEFAULT_COLUMN_WIDTH = 20; // rem
--- srcApp/utility/getColumnWidth.js
+++ srcApp/utility/getColumnWidth.js
@@ -1,9 +1,11 @@
-import {HALF_GAP, MIN_COLUMN_NO, DEFAULT_COLUMN_WIDTH} from '../config';
+import {
+    HALF_GAP, MIN_COLUMN_NO, DEFAULT_COLUMN_WIDTH, WIW_THRESHOLD
+} from '../config';
 
 // 当宽度小到一定程度，判断为手机用户，尽量占满屏幕
 export default (fs, wiw) => {
-    // 58rem大概是900px。小于这个值，列数不变，但是把空白位置占满。
-    if (wiw < 900) {
+    // 小于这个门限值，列数不变，但是把空白位置占满。
+    if (wiw <= WIW_THRESHOLD) {
         const completeColumnNo = Math.floor((wiw - 2 * HALF_GAP * fs)
             / (DEFAULT_COLUMN_WIDTH * fs));
         const defaultColumnNo = Math.max(MIN_COLUMN_NO, completeColumnNo);
