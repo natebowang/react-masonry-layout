d3c38c36866dea38930ae96b736b43810e09b624
natebowang <120029790@qq.com>
2019-07-03 17:20:08 +0800

    Fixed bugs:
    Modify completeColumnNo back in getColumnNo. The last commit is not right.
    Adjust config.

--- srcApp/config.js
+++ srcApp/config.js
@@ -1,13 +1,17 @@
 // half half between cells in masonry layout
-export const HALF_GAP = 0.6; // rem
+export const HALF_GAP = 0.5; // rem
 
 // Column number can not less than this value
 export const MIN_COLUMN_NO = 1;
 
 // If window inner width less than this, column width will
 // larger than DEFAULT_COLUMN_WIDTH to extend to the maximum use of the window.
-export const WIW_THRESHOLD = 730; // px
+export const WIW_THRESHOLD = 925; // px,
+// One column: full wiw<=700,
+// Two columns: full 700<wiw<=925, not full 925<wiw<=1372
+// Three columns: not full 1372<wiw<=1820
+// Four columns: not full 1820<wiw<=1920
 
-export const DEFAULT_COLUMN_WIDTH = 22; // rem
+export const DEFAULT_COLUMN_WIDTH = 28; // rem
 
 export const SCROLL_BAR_WIDTH = 0.8; // rem
--- srcApp/utility/getColumnNo.js
+++ srcApp/utility/getColumnNo.js
@@ -2,7 +2,7 @@ import {HALF_GAP, MIN_COLUMN_NO, SCROLL_BAR_WIDTH} from '../config';
 import getColumnWidth from './getColumnWidth';
 
 export default (fs, wiw) => {
-    const completeColumnNo = Math.round((wiw / fs - 2 * HALF_GAP - SCROLL_BAR_WIDTH)
+    const completeColumnNo = Math.floor((wiw / fs - 2 * HALF_GAP - SCROLL_BAR_WIDTH)
         / (getColumnWidth(fs, wiw)));
     // 最小也得是1列，不能返回0列
     return Math.max(MIN_COLUMN_NO, completeColumnNo);
